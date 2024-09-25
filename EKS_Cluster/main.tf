fe830a51647a9555c939fff29bc8cd60c6578ef8
Gatabaki <120761479+Gatabaki@users.noreply.github.com>
2024-09-24 22:38:45 -0500

    Update main.tf

--- EKS_Cluster/main.tf
+++ EKS_Cluster/main.tf
@@ -1,14 +1,14 @@
-# VPC
+# VPC MODULE
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "EKS-VPC"
+  name = "jenkins-vpc"
   cidr = var.vpc_cidr
 
-  azs             = data.aws_availability_zones.azs.names
-  public_subnets  = var.public_subnets
-  private_subnets = var.private_subnets
-
+  azs                  = data.aws_availability_zones.azs.names
+  private_subnets      = var.private_subnets
+  public_subnets       = var.public_subnets
   enable_dns_hostnames = true
   enable_nat_gateway   = true
   single_nat_gateway   = true
@@ -25,40 +25,34 @@ module "vpc" {
   private_subnet_tags = {
     "kubernetes.io/cluster/my-eks-cluster" = "shared"
     "kubernetes.io/role/internal-elb"      = 1
-
   }
-
 }
 
-# EKS
+# EKS MODULE
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.24"
-
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  # EKS Managed Node Group(s)
+  cluster_name                   = "my-eks-cluster"
+  cluster_version                = "1.30"
+  cluster_endpoint_public_access = true
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
 
   eks_managed_node_groups = {
-    nodes = {
+    node = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t2.small"]
+
       min_size     = 1
       max_size     = 3
       desired_size = 2
-
-      instance_type = ["t2.small"]
     }
   }
+
   tags = {
     Environment = "dev"
     Terraform   = "true"
   }
 }
-  
-
-
-  # Pushing to the limit
