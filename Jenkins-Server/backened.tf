978272423d7975e0e3c5590c8aef3dab922a83f5
Gatabaki <120761479+Gatabaki@users.noreply.github.com>
2024-09-24 22:32:15 -0500

    Update backened.tf

--- Jenkins-Server/backened.tf
+++ Jenkins-Server/backened.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "gatabaki-terraform-eks-project"
-    key    = "jenkins/terraform.tfstate"
+    bucket = "eks-terraform-project"
+    key    = "eks project/terraform.tfstate"
     region = "us-east-2"
   }
 }
